89ebefb98ee21546b148792bdf20509b5fc803bb
tjhv10 <achiyabennatan.com>
2025-01-12 09:24:35 +0200

    ok

--- src/components/items/hooks/useSetItemsCategory.tsx
+++ src/components/items/hooks/useSetItemsCategory.tsx
@@ -1,86 +1,68 @@
-import { Dispatch, SetStateAction, useEffect } from "react";
-// import md from "../../../assets/MOCK_DATA.json";
-import { ButtonsEnum, CartItem } from "../../../Store/shopping-cart-context";
-import { subcategoryEnum, DateEnum } from "./useSetFilterChoose";
-import { ItemProps } from "../../Item/item";
-export enum CategoryEnum {
-  "No filter" = "No filter",
-  "Category" = "Category",
-  "Uploaded date" = "Uploaded date",
-  "Price" = "Price",
-}
-function useSetItemsCategory(
-  items: { [x: string]: ItemProps[] },
-  addItem: (item: CartItem) => void,
-  subcategory: subcategoryEnum | DateEnum | number[] | undefined,
-  setItems: Dispatch<SetStateAction<CartItem[]>>,
-  category: string,
-  setSubcategory: Dispatch<subcategoryEnum | DateEnum | number[] | undefined>,
-  loading: boolean
-) {
-  useEffect(() => {
-    if (loading) {
-      return;
-    }
-    setItems([]);
-    const sortedItems = Object.values(items)
-      .flat()
-      .sort((a: ItemProps, b: ItemProps) => a.id - b.id);
-    for (const sortedItem of sortedItems) {
-      const item: CartItem = {
-        product: sortedItem,
-        quantity: 1,
-        buttons: new Map<ButtonsEnum, boolean>([
-          [ButtonsEnum.AddToCartAndGoToItemPage, true],
-        ]),
-      };
+// import { Dispatch, SetStateAction, useEffect } from "react";
+// // import md from "../../../assets/MOCK_DATA.json";
+// import { ButtonsEnum, CartItem } from "../../../Store/shopping-cart-context";
+// import { ItemProps } from "../../Item/item";
+// import { CategoryEnum, DateEnum } from "../enums";
 
-      switch (category) {
-        case CategoryEnum["No filter"]: {
-          addItem(item);
-          break;
-        }
-        case CategoryEnum["Category"]: {
-          if (
-            item.product.categories.some(
-              (category) => category.name === subcategory
-            )
-          ) {
-            addItem(item);
-          }
-          break;
-        }
-        case CategoryEnum["Price"]: {
-          if (Array.isArray(subcategory) && subcategory.length === 2) {
-            const [lowPrice, highPrice] = subcategory;
-            if (
-              item.product.price >= lowPrice &&
-              item.product.price <= highPrice
-            )
-              addItem(item);
-          }
-          break;
-        }
-        case CategoryEnum["Uploaded date"]: {
-          if (
-            typeof subcategory === "string" &&
-            Date.parse(item.product.uploadDate) >= Date.parse(subcategory!)
-          )
-            addItem(item);
-          break;
-        }
-      }
-      switch (category) {
-        case CategoryEnum["No filter"]: {
-          setSubcategory(
-            subcategoryEnum["No filter" as keyof typeof subcategoryEnum]
-          );
-          break;
-        }
-      }
-    }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [loading, category, subcategory]);
-}
+// function useSetItemsCategory(
+//   items: { [x: string]: ItemProps[] },
+//   addItem: (item: CartItem) => void,
+//   setItems: Dispatch<SetStateAction<CartItem[]>>,
+//   category: string,
+//   setSubcategory: Dispatch<CategoryEnum | DateEnum | number[] | undefined>,
+//   loading: boolean
+// ) {
+//   useEffect(() => {
+//     if (loading) {
+//       return;
+//     }
+//     setItems([]);
+//     const sortedItems = Object.values(items)
+//       .flat()
+//       .sort((a: ItemProps, b: ItemProps) => a.id - b.id);
+//     for (const sortedItem of sortedItems) {
+//       const item: CartItem = {
+//         product: sortedItem,
+//         quantity: 1,
+//         buttons: new Map<ButtonsEnum, boolean>([[ButtonsEnum.AddToCartAndGoToItemPage, true]]),
+//       };
 
-export default useSetItemsCategory;
+//       switch (category) {
+//         case CategoryEnum["No filter"]: {
+//           addItem(item);
+//           break;
+//         }
+//         case CategoryEnum["Category"]: {
+//           if (item.product.categories.some((category) => category.name === subcategory)) {
+//             addItem(item);
+//           }
+//           break;
+//         }
+//         case CategoryEnum["Price"]: {
+//           if (Array.isArray(subcategory) && subcategory.length === 2) {
+//             const [lowPrice, highPrice] = subcategory;
+//             if (item.product.price >= lowPrice && item.product.price <= highPrice) addItem(item);
+//           }
+//           break;
+//         }
+//         case CategoryEnum["Uploaded date"]: {
+//           if (
+//             typeof subcategory === "string" &&
+//             Date.parse(item.product.uploadDate) >= Date.parse(subcategory!)
+//           )
+//             addItem(item);
+//           break;
+//         }
+//       }
+//       switch (category) {
+//         case CategoryEnum["No filter"]: {
+//           setSubcategory(subcategoryEnum["No filter" as keyof typeof subcategoryEnum]);
+//           break;
+//         }
+//       }
+//     }
+//     // eslint-disable-next-line react-hooks/exhaustive-deps
+//   }, [loading, category, subcategory]);
+// }
+
+// export default useSetItemsCategory;
--- src/components/items/items.tsx
+++ src/components/items/items.tsx
@@ -1,62 +1,49 @@
 import React, { useRef, useState } from "react";
 import styles from "./items.module.scss";
 import Item from "../Item/item.tsx";
-import useSetItemsCategory, {
-  CategoryEnum,
-} from "./hooks/useSetItemsCategory.tsx";
-import useSetFilterChoose, {
-  subcategoryEnum,
-  DateEnum,
-} from "./hooks/useSetFilterChoose.tsx";
 import useSort, { SortEnum } from "./hooks/useSort.tsx";
 import searchFunction from "./functions/searchFunction.tsx";
 import { ButtonsEnum, CartItem } from "../../Store/shopping-cart-context.tsx";
 import { useGetItems } from "../../fetchDataQueries/useGetItems.tsx";
+import { Slider } from "@mui/material";
+import { CategoryEnum, DateEnum } from "./enums.tsx";
 
 const Items: React.FC = () => {
-  const [category, setCategory] = useState<CategoryEnum>(
-    CategoryEnum["No filter"]
-  );
-  const [subcategory, setSubcategory] = useState<
-    subcategoryEnum | number[] | DateEnum
-  >();
+  const [priceRange, setPriceRange] = useState<number[]>([0, 2000]);
   const [Items, setItems] = useState<CartItem[]>([]);
 
   const [sort, setSort] = useState<SortEnum>(SortEnum.id);
   const selectSortRef = useRef<HTMLSelectElement>(null);
   const selectFilterRef = useRef<HTMLSelectElement>(null);
-  const options = useSetFilterChoose(category, subcategory, setSubcategory);
   const addItem = (Item: CartItem) => {
     setItems((prevItems) => [...prevItems, Item]);
   };
-
+  const handleChange = (_event: Event, newValue: number | number[]) => {
+    if (Array.isArray(newValue) && newValue.length === 2) {
+      const [lowPrice, highPrice] = newValue;
+      setPriceRange(newValue);
+      for (const item of Items) {
+        if (item.product.price >= lowPrice && item.product.price <= highPrice) addItem(item);
+      }
+    }
+  };
+  function valuetext(value: number) {
+    return `${value}$`;
+  }
   useSort(sort, setItems);
   const { data, loading } = useGetItems();
-  useSetItemsCategory(
-    data,
-    addItem,
-    subcategory,
-    setItems,
-    category,
-    setSubcategory,
-    loading
-  );
   if (loading) return "Loading...";
   let alli: CartItem[] = [];
   for (let i = 0; i < data.length; i++) {
     const item: CartItem = {
       product: data[i],
       quantity: 1,
-      buttons: new Map<ButtonsEnum, boolean>([
-        [ButtonsEnum.AddToCartAndGoToItemPage, true],
-      ]),
+      buttons: new Map<ButtonsEnum, boolean>([[ButtonsEnum.AddToCartAndGoToItemPage, true]]),
     };
     alli.push(item);
   }
 
-  alli = alli
-    .slice()
-    .sort((a: CartItem, b: CartItem) => a.product.id - b.product.id);
+  alli = alli.slice().sort((a: CartItem, b: CartItem) => a.product.id - b.product.id);
 
   return (
     <div className={styles.page}>
@@ -74,8 +61,6 @@ const Items: React.FC = () => {
                 setSort(SortEnum.id);
               }
               if (selectFilterRef.current) {
-                selectFilterRef.current.value = CategoryEnum["No filter"];
-                setCategory(CategoryEnum["No filter"]);
                 selectFilterRef.current.selectedIndex = 0;
               }
             } else {
@@ -83,29 +68,60 @@ const Items: React.FC = () => {
             }
           }}
         />
+        Category:
         <select
-          ref={selectFilterRef}
           className={styles.filtersBarItem}
-          onChange={(e) =>
-            setCategory(
-              CategoryEnum[e.target.value as keyof typeof CategoryEnum]
-            )
-          }
-        >
-          {Object.values(CategoryEnum).map((filter) => (
-            <option value={filter} key={filter}>
-              {filter}
+          onChange={(e) => {
+            for (const item of Items) {
+              if (
+                e.currentTarget.value === "All" ||
+                item.product.categories.some((category) => category.name === e.currentTarget.value)
+              ) {
+                addItem(item);
+              }
+            }
+          }}>
+          {Array.from(new Set(Object.values(CategoryEnum))).map((option) => (
+            <option value={option} key={option}>
+              {option}
             </option>
           ))}
         </select>
-        {category !== CategoryEnum["No filter"] ? options : null}
+        Uploaded date from:
+        <select
+          className={styles.filtersBarItem}
+          onChange={(e) => {
+            for (const item of Items) {
+              if (
+                e.currentTarget.value === "All Dates" ||
+                Date.parse(item.product.uploadDate) >= Date.parse(e.currentTarget.value)
+              )
+                addItem(item);
+            }
+          }}>
+          {Array.from(new Set(Object.values(DateEnum))).map((option) => (
+            <option value={option} key={option}>
+              {option}
+            </option>
+          ))}
+        </select>
+        Price range:
+        <Slider
+          getAriaLabel={() => "Price range"}
+          value={priceRange}
+          onChange={handleChange}
+          valueLabelDisplay="auto"
+          getAriaValueText={valuetext}
+          className={styles.filtersBarItemPrice}
+          min={0}
+          max={2000}
+        />
         <select
           ref={selectSortRef}
           className={styles.filtersBarItem}
           onChange={(e) => {
             setSort(SortEnum[e.currentTarget.value as SortEnum]);
-          }}
-        >
+          }}>
           <option value="id">Sort...</option>
           <option value="price">Sort by price</option>
           <option value="date">Sort by uploaded date</option>

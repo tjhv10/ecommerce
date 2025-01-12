d20dc02b2ee00224d3106e916d4ff64ca5f07a45
tjhv10 <achiyabennatan.com>
2025-01-12 16:59:27 +0200

    refactor: move GraphQL query definition outside the hook and improve variable handling in useGetItemById

--- src/fetchDataQueries/useGetItemById.tsx
+++ src/fetchDataQueries/useGetItemById.tsx
@@ -1,26 +1,33 @@
 import { gql, useQuery } from "@apollo/client";
-export function useGetItemById(id: number) {
-  const GET_ITEM_BY_ID = gql`
-      query {
-        getItemById(id: ${id}) {
-          id
-          name
-          price
-          uploadDate
-          description
-          sellerName
-          status
-          imageUrl
-          categories {
-            id
-            name
-          }
-        }
+
+// Define the query outside the hook
+const GET_ITEM_BY_ID = gql`
+  query getItemById($id: Int!) {
+    getItemById(id: $id) {
+      id
+      name
+      price
+      uploadDate
+      description
+      sellerName
+      status
+      imageUrl
+      categories {
+        id
+        name
       }
-    `;
-  const { loading, error, data } = useQuery(GET_ITEM_BY_ID);
+    }
+  }
+`;
+
+export function useGetItemById(id: number) {
+  const { loading, error, data } = useQuery(GET_ITEM_BY_ID, {
+    variables: { id },
+  });
+
   if (error) {
     console.error(`Error! ${error.message}`);
   }
+
   return { data: data ? data.getItemById : null, loading };
 }
